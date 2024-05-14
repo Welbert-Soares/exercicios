5c57c6d163914a1ef79ba88d5800bf0981833acd
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-14 17:28:36 -0300

    renderização condicional

---
+++ pages/basicos/componenteComFilho.jsx
@@ -0,0 +1,17 @@
+import Item from "@/components/Item";
+import Lista from "@/components/Lista";
+
+export default function componenteComFilho() {
+    return (
+        <div>
+            <Lista>
+                <Item conteudo="Item 1"></Item>
+                <Item conteudo="Item 2"></Item>
+                <Item conteudo="Item 3"></Item>
+                <Item conteudo="Item 11"></Item>
+                <Item conteudo="Item 12"></Item>
+                <Item conteudo="Item 13"></Item>
+            </Lista>
+        </div>
+    )
+}
