203429ca1ae3b020eda68e8486fdb77744b17b02
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-14 15:57:50 -0300

    renderizacao

---
+++ data/listaProdutos.js
@@ -0,0 +1,9 @@
+import Produto from "@/model/produto"
+
+
+export default [
+    new Produto(1, "iPad", 7543.99),
+    new Produto(2, "Monitor 24", 949.99),
+    new Produto(3, "Computador Gamer", 6949.99),
+    new Produto(4, "Samsung S20", 2800.0),
+]
