c0194f46d4a81848b0c6b4e7e33b895f5142cd43
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-14 11:41:49 -0300

    repeticao

---
+++ pages/render/repeticao1.jsx
@@ -0,0 +1,27 @@
+export default function Repeticao1() {
+    const listaAprovados = [
+        'João',
+        'Maria',
+        'Ana',
+        'Bia',
+        'Carlos',
+        'Daniel',
+        'Laura'
+    ]
+
+    function renderizarLista() {
+        return (
+            <>
+                
+            </>
+        )
+    }
+
+    return (
+        <ul>
+            <li>Elemento 1</li>
+            <li>Elemento 2</li>
+            <li>Elemento 3</li>
+        </ul>
+    )
+}
