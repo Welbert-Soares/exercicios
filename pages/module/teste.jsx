d53b334197a33b6818e573b42bd7c210afe4167e
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-15 20:37:43 -0300

    funcionais

---
+++ pages/module/teste.jsx
@@ -0,0 +1,11 @@
+import Comp1 from "../../components/module/funcionais";
+
+function teste() {
+    return ( 
+        <div>
+            <Comp1 />
+        </div>
+     );
+}
+
+export default teste;
