161acd59ebcd32c51284918863ac9237b80166f2
Welbert Soares <139386609+Welbert-Soares@users.noreply.github.com>
2024-05-15 11:26:33 -0300

    componente com estado

---
+++ pages/estado/mouse.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+
+function mouse() {
+    const [x, setX] = useState(0); // usando array destructuring
+   
+    const arrayY = useState(0); // modo menos comum
+    let y = arrayY[0];
+    const alterarY = arrayY[1];
+
+    const estilo = {
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#222",
+        color: "#fff",
+        height: "100vh",
+    };
+    function quandoMover(event) {
+        setX(event.clientX);
+        alterarY(event.clientY);
+    }
+    return (
+        <div style={estilo} onMouseMove={quandoMover}>
+            <span>Eixo X: {x}</span>
+            <span>Eixo y: {y}</span>
+        </div>
+    );
+}
+
+export default mouse;
